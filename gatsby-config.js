00ae31b2bf0bc29ec162b4f819379dec12720f61
Michal Krzyzanowski <michal.krzyzanowski@gmail.com>
2020-06-09 11:57:59 +0200

    Add fb pixel

--- gatsby-config.js
+++ gatsby-config.js
@@ -47,7 +47,13 @@ module.exports = {
           },
         ],
       },
-    }
+    },
+    {
+      resolve: `gatsby-plugin-facebook-pixel`,
+      options: {
+        pixelId: '677673589754274',
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
